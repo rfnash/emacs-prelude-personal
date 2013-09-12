6919a9599ace4091a0f90ab87daadf2b0609ca8d
Robert F. Nash <robert@robertnash.net>
2013-09-11 20:22:35 -0400

    moved init.el ansi-term settings to prelude

--- personal.el
+++ personal.el
@@ -26,6 +26,33 @@
 (color-theme-solarized-light)
 (set-face-attribute 'default nil :family "Liberation Mono for Powerline" :height 120)
 
+;;; ansi-term solarized settings
+
+(setq term-default-bg-color "#002b36"
+      term-default-fg-color "#eee8d5"
+      term-mode-hook '((lambda nil
+                         (color-theme-buffer-local
+                          (quote color-theme-solarized-dark)
+                          (current-buffer))))
+      term-scroll-to-bottom-on-output t)
+
+;; TODO: is probably a bad idea to set this here. Find a better way.
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(term ((t (:inherit default :background "#002b36"))))
+ '(term-bold ((t (:inverse-video t :weight bold))))
+ '(term-color-black ((t (:background "#002b36" :foreground "#073642"))))
+ '(term-color-blue ((t (:background "#002b36" :foreground "#268bd2"))))
+ '(term-color-cyan ((t (:background "#002b36" :foreground "#2aa198"))))
+ '(term-color-green ((t (:background "#002b36" :foreground "#859900"))))
+ '(term-color-magenta ((t (:background "black" :foreground "#d33682"))))
+ '(term-color-red ((t (:background "#002b36" :foreground "#dc322f"))))
+ '(term-color-white ((t (:background "#002b36" :foreground "#eee8d5"))))
+ '(term-color-yellow ((t (:background "#002b36" :foreground "#b58900")))))
+
 ;;; Configure helm
 
 (require 'helm-config)

b272744a777342bd3b830d85bdf1439e97b43c4e
Robert F. Nash <robert@robertnash.net>
2013-09-17 23:26:57 -0400

    started tracking custimizations made using customize interface

---
+++ custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(fci-rule-color "#383838")
+ '(org-refile-use-cache t)
+ '(prelude-clean-whitespace-on-save nil)
+ '(prelude-whitespace nil)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(term ((t (:inherit default :background "#002b36"))))
+ '(term-bold ((t (:inverse-video t :weight bold))))
+ '(term-color-black ((t (:background "#002b36" :foreground "#073642"))))
+ '(term-color-blue ((t (:background "#002b36" :foreground "#268bd2"))))
+ '(term-color-cyan ((t (:background "#002b36" :foreground "#2aa198"))))
+ '(term-color-green ((t (:background "#002b36" :foreground "#859900"))))
+ '(term-color-magenta ((t (:background "black" :foreground "#d33682"))))
+ '(term-color-red ((t (:background "#002b36" :foreground "#dc322f"))))
+ '(term-color-white ((t (:background "#002b36" :foreground "#eee8d5"))))
+ '(term-color-yellow ((t (:background "#002b36" :foreground "#b58900")))))

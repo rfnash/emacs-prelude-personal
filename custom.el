dd4771f7859ceb11bb680b86c4bada8bfa80d02d
Robert F. Nash <robert@robertnash.net>
2014-05-09 16:23:43 -0400

    custom.el: removed unecessary customization of yas-snippit-dirs

--- custom.el
+++ custom.el
@@ -15,8 +15,7 @@
  '(prelude-whitespace nil)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3")
- '(yas-snippet-dirs (quote ("~/.emacs.d/prelude/personal/snippits" "/home/rfnash/.emacs.d/prelude/elpa/yasnippet-20140106.1009/snippets")) nil (yasnippet)))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

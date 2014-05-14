c394c5321ff0ad79c7f8594ba43b59b9db447f3a
Robert F. Nash <robert@robertnash.net>
2014-05-14 00:15:04 -0400

    mu4e: don't run myfdm when indexing new mail

--- rfnash-mu4e.el
+++ rfnash-mu4e.el
@@ -12,7 +12,7 @@
       mu4e-drafts-folder "/Drafts"         ;; unfinished messages
       mu4e-trash-folder  "/Trash"          ;; trashed messages
       mu4e-refile-folder "/archive"        ;; saved messages
-      mu4e-get-mail-command "myfdm")
+      mu4e-get-mail-command "")
 (add-to-list 'mu4e-view-actions
                                         ; Open message in external browser with 'x' as the shortcut
              '("xview in browser" . mu4e-action-view-in-browser) t)

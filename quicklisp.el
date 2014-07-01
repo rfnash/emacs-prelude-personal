ed4c2b6b022e392f7f46605b404171fafe84afe4
Robert F. Nash <robert@robertnash.net>
2014-07-01 14:41:06 -0400

    moved quicklisp into personal.org

--- quicklisp.el
+++
@@ -1,5 +0,0 @@
-;; Added manually as per quicklisp instructed
-;; after running (ql:quickload "quicklisp-slime-helper") in sblc
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-

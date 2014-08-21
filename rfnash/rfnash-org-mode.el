738b2d3c6b20575df5c7c6c917daca0b7ae8796a
Robert F. Nash <robert@robertnash.net>
2014-08-21 11:26:53 -0400

    org-mode: use locally installed version

--- rfnash/rfnash-org-mode.el
+++ rfnash/rfnash-org-mode.el
@@ -16,8 +16,9 @@
 ;; TODO: org-mobile-sync requires unavailable package emacs 24.3.50
 (prelude-require-packages '(org-pomodoro org-trello))
 (require 'rfnash-el-get)
-;;(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
+(add-to-list 'load-path "~/.local/share/emacs/site-lisp/org")
 (add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp")
+(load-file "~/.local/share/emacs/site-lisp/org/org-compat.el")
 (require 'org)
 (require 'rfnash-org-modules)
 

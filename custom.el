28861985b94b29244f2b8b4bcc84c8c9efcf18d9
Robert F. Nash <robert@robertnash.net>
2015-03-03 00:11:15 -0500

    custom.el: fix Tex programs

--- custom.el
+++ custom.el
@@ -5,6 +5,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(paradox-automatically-star t)
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "xdg-open")
+     (output-html "xdg-open"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
